4d353dc8ff8dd4d91165ecac0b85fa68a534efd2
shtramak <shtramak@gmail.com>
2019-04-08 17:58:42 +0300

    implement account-dao exercise
    to be refactored with helper methods to avoid code duplication

--- account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/ua/procamp/dao/AccountDaoImpl.java
@@ -1,7 +1,9 @@
 package ua.procamp.dao;
 
+import ua.procamp.exception.AccountDaoException;
 import ua.procamp.model.Account;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
@@ -14,32 +16,96 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.persist(account);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public Account findById(Long id) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            Account account = entityManager.find(Account.class, id);
+            entityManager.getTransaction().commit();
+            return account;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
+
     }
 
     @Override
     public Account findByEmail(String email) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            return entityManager
+                    .createQuery("SELECT a FROM Account a WHERE a.email=:email", Account.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public List<Account> findAll() {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            return entityManager.createQuery("from Account", Account.class).getResultList();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void update(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.merge(account);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void remove(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            Account managedAccount = entityManager.merge(account);
+            entityManager.remove(managedAccount);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("Exception", e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
 
