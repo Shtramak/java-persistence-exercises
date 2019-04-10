35c93cfb4c5a6ac34ea3134a3a8bfb17a8856d68
shtramak <shtramak@gmail.com>
2019-04-10 20:11:55 +0300

    implement company-products exercise

--- company-products/src/main/java/ua/procamp/dao/CompanyDaoImpl.java
+++ company-products/src/main/java/ua/procamp/dao/CompanyDaoImpl.java
@@ -1,8 +1,12 @@
 package ua.procamp.dao;
 
+import org.hibernate.Session;
+import ua.procamp.exception.CompanyDaoException;
 import ua.procamp.model.Company;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.function.Function;
 
 public class CompanyDaoImpl implements CompanyDao {
     private EntityManagerFactory entityManagerFactory;
@@ -13,6 +17,25 @@ public class CompanyDaoImpl implements CompanyDao {
 
     @Override
     public Company findByIdFetchProducts(Long id) {
-        throw new UnsupportedOperationException("I'm still not implemented!");
+        return executeWithinPersistenceContextReturningResult(entityManager -> entityManager
+                .createQuery("SELECT c FROM Company c left join fetch c.products WHERE c.id=:id", Company.class)
+                .setParameter("id", id)
+                .getSingleResult());
+    }
+
+    private <T> T executeWithinPersistenceContextReturningResult(Function<EntityManager, T> emFuntion) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.unwrap(Session.class).setDefaultReadOnly(true);
+        entityManager.getTransaction().begin();
+        try {
+            T result = emFuntion.apply(entityManager);
+            entityManager.getTransaction().commit();
+            return result;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new CompanyDaoException("Exception occurred during transaction...", e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
