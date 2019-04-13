9af74d769bbf52fec174c7033b168f4b87bb1e12
shtramak <shtramak@gmail.com>
2019-04-13 22:35:40 +0300

    implement employee-profile exercise

--- employee-profile/src/main/java/ua/procamp/model/EmployeeProfile.java
+++ employee-profile/src/main/java/ua/procamp/model/EmployeeProfile.java
@@ -1,28 +1,46 @@
 package ua.procamp.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 /**
  * todo:
  * - implement not argument constructor
  * - implement getters and setters
  * - implement equals and hashCode based on identifier field
- *
+ * <p>
  * - configure JPA entity
  * - specify table name: "employee_profile"
  * - configure not nullable columns: position, department
- *
+ * <p>
  * - map relation between {@link Employee} and {@link EmployeeProfile} using foreign_key column: "employee_id"
  * - configure a derived identifier. E.g. map "employee_id" column should be also a primary key (id) for this entity
  */
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "employee_profile")
 public class EmployeeProfile {
+    @Id
     private Long id;
+    @OneToOne(optional = false)
+    @JoinColumn(name = "employee_id")
+    @MapsId
     private Employee employee;
+    @Column(nullable = false)
     private String position;
+    @Column(nullable = false)
     private String department;
 }
